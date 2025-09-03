2a0e73b2d9c3be1f679984984277da8d8aad6ea0
Drossano <72749793+drossano@users.noreply.github.com>
2025-09-03 01:54:25 +0000

    chore: change node devcontainer version to bullseye

--- backend/src/test/globalSetup.js
+++ backend/src/test/globalSetup.js
@@ -2,7 +2,7 @@ import { MongoMemoryServer } from 'mongodb-memory-server'
 export default async function globalSetup() {
   const instance = await MongoMemoryServer.create({
     binary: {
-      version: '8.0.10',
+      version: '6.0.13',
     },
   })
   global.__MONGOINSTANCE = instance
