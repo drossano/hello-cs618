271448bc6e0f24425b476630ef535ae9e21c82ff
Drossano <72749793+drossano@users.noreply.github.com>
2025-09-09 22:53:41 +0000

    feat: add filtering & sorting

--- backend/src/example.js
+++ backend/src/example.js
@@ -1,5 +1,8 @@
 import { initDatabase } from './db/init.js'
 import { Post } from './db/models/post.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
 await initDatabase()
 
 const post = new Post({
@@ -9,11 +12,7 @@ const post = new Post({
   tags: ['mongoose', 'mongodb'],
 })
 
-const createdPost = await post.save()
-
-await Post.findByIdAndUpdate(createdPost._id, {
-  $set: { title: 'Hello again, Mongoose!' },
-})
+await post.save()
 
 const posts = await Post.find()
 console.log(posts)
--- src/api/posts.js
+++ src/api/posts.js
@@ -1,6 +1,6 @@
 export const getPosts = async (queryParams) => {
   const res = await fetch(
-    `${import.meta.env.VITE_BACKEND_URL}/posts` +
+    `${import.meta.env.VITE_BACKEND_URL}/posts?` +
       new URLSearchParams(queryParams),
   )
   return await res.json()
