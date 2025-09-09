9549a1371042669ede2a30a52240b6388545906f
Drossano <72749793+drossano@users.noreply.github.com>
2025-09-09 02:13:46 +0000

    feat: fetch posts from backend

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,7 +1,9 @@
+import { useQuery } from "@tanstack/react-query"
 import { CreatePost } from "./components/CreatePost.jsx"
 import { PostFilter } from "./components/PostFilter.jsx"
 import { PostList } from "./components/PostList.jsx"
 import { PostSorting } from "./components/PostSorting.jsx"
+import { getPosts } from "./api/posts.js"
 
 const posts = [
 {
@@ -13,6 +15,14 @@ author: 'Daniel Bugl',
 ]
 
 export function Blog() {
+
+
+  const postsQuery = useQuery({
+    queryKey: ['posts'],
+    queryFn: () => getPosts(),
+  })
+
+  const posts = postsQuery.data ?? []
   return (
     <div style={{ padding: 8 }}>
       <CreatePost />
